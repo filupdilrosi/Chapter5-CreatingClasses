40ade954eb71a449ce7a4ff061c9f8419c11e6e5
Phillip DelRossi <101755335+filupdilrosi@users.noreply.github.com>
2022-08-31 17:57:53 -0400

    Add files via upload

---
+++ RationalNumbersClassDependency/RationalTester.java
@@ -0,0 +1,31 @@
+package Chapters5BuildingClasses.RationalTesterClassDependency;
+//Driver class to exercise the use of multiple rational objects
+public class RationalTester {
+    //Creates some rational number objects and preforms various operations on them
+    public static void main(String[] args) {
+        RationalNumber r1 = new RationalNumber(6,8);
+        RationalNumber r2 = new RationalNumber(1,3);
+        RationalNumber r3, r4, r5, r6, r7;
+
+        System.out.println("First rational number: " + r1);
+        System.out.println("Second rational number: " + r2);
+
+        if (r1.isLike(r2))
+            System.out.println("r1 and r2 are equal");
+        else
+            System.out.println("r1 and r2 are not equal.");
+
+        r3 = r1.reciprocal();
+        System.out.println("The reciprocal of r1 is: " + r3);
+
+        r4 = r1.add(r2);
+        r5 = r1.subtract(r2);
+        r6 = r1.multiply(r2);
+        r7 = r1.divide(r2);
+
+        System.out.println("r1 + r2: " + r4);
+        System.out.println("r1 - r2: " + r5);
+        System.out.println("r1 * r2: " + r6);
+        System.out.println("r1 / r2: " + r7);
+    }
+}
